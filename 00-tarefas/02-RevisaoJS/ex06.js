e83b069b4fa780fb3be14f72e8ee153a295d280e
wellingtonpvd244-cyber <wellington.pvd.244@gmail.com>
2026-07-09 14:33:33 -0300

    atividade realizade07

--- 00-tarefas/02-RevisaoJS/ex06.js
+++ 00-tarefas/02-RevisaoJS/ex06.js
@@ -7,3 +7,21 @@
 // A pessoa possui somente R$30, após a operação verifique se a pessoa possui dinheiro suficeinte
 
 // Mostre o preço final juntamente com a possibilidae da pessoa poder ou não comprar o produto.
+
+let resultado = 120;
+let desconto = 0.2;
+let  dinheirodisponivel = 30;
+let  preçofinal = resultado * (1 - desconto);
+
+console.log("preçofinal do produto: %d preçofinal *dinheirodisponivel");
+
+if(dinheirodisponivel >= preçofinal){
+    
+ console.log("Compra aprovada!");
+
+}else{
+
+
+    console.log("Dinheiro insuficiente");
+}
+
--- 00-tarefas/02-RevisaoJS/ex07.js
+++ 00-tarefas/02-RevisaoJS/ex07.js
@@ -11,3 +11,15 @@
 // >=5 Recuperação
 
 // Abaixo de 5 Reprovado
+
+const nota = 9;
+
+if(nota >=9){
+    console.log("excelente");
+}else if (nota >=7){
+    console.log("bom");
+}else if (nota >=5){
+    console.log("recuperacao");
+}else {
+    console.log("aprovado"); 
+}
